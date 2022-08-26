365d02f5df6a38747a8c32df660078b77f7894e1
AHSAN MDT <ahnkhan804@gmail.com>
2022-08-26 18:50:33 +0500

    UPdated!

---
+++ JRCar.WebApp/NotificationComponent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Configuration;
+
+namespace JRCar.WebApp
+{
+    public class NotificationComponent
+    {
+        public void RegisterNotification(DateTime currentTime)
+        {
+            string constring = ConfigurationManager.ConnectionStrings["jrcarNotification"].ConnectionString;
+            string SqlCommand = @"";
+        }
+    }
+}
---
+++ JRCar.WebApp/Startup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(JRCar.WebApp.Startup))]
+
+namespace JRCar.WebApp
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
